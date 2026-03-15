e061bc46c651c070be62a91292b53987fce9f5cf
aamandaps <aamandaps01@gmail.com>
2026-03-15 17:25:37 -0300

    Add files via upload

---
+++ 01area_quadrado.py
@@ -0,0 +1,9 @@
+#declaração das variáveis
+lado: int
+area: int
+
+#inicio
+lado = int(input("Insira o tamanho de um lado do quadrado "))
+area = lado * lado
+print("Área do quadrado =", area)
+#fim
